519974513fa9abd25bd7e152def92aa4b65a4dca
AhmadHaikalHarahap <haikalbae1216@gmail.com>
2026-03-31 16:07:15 +0700

    Add files via upload

---
+++ latihan/src/MainApp.java
@@ -0,0 +1,21 @@
+// Class untuk menjalankan program
+public class MainApp {
+    public static void main(String[] args) {
+
+        // Membuat objek
+        MesinKopi mesinRuangTunggu = new MesinKopi();
+
+        // Cek awal
+        System.out.println("Mesin siap? " + mesinRuangTunggu.siapCappuccino());
+
+        // Isi bahan
+        mesinRuangTunggu.tambahBahan(120, 400, 250);
+
+        // Cek lagi
+        if (mesinRuangTunggu.siapCappuccino()) {
+            System.out.println("Mesin siap digunakan.");
+        } else {
+            System.out.println("Bahan belum cukup.");
+        }
+    }
+}
