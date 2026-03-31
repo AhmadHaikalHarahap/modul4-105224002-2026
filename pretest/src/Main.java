cd8bf4c61e9ddc1beff894aa6bdb3c4d1db8d7f8
AhmadHaikalHarahap <haikalbae1216@gmail.com>
2026-03-31 15:02:18 +0700

    pretest

---
+++ pretest/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Mahasiswa mhsBaru = new Mahasiswa("Ahmad Haikal Harahap", 17, "Informatika");
+        mhsBaru.tampilkanData();
+    }
+}
---
+++ pretest/src/mhsBaru.java
@@ -0,0 +1,8 @@
+public class mhsBaru {
+    public static void main(String[] args) {
+        Mahasiswa mhsBaru = new Mahasiswa("Haikal", 20, "Informatika");
+        System.out.println("Nama: " + mhsBaru.nama);
+        System.out.println("Umur: " + mhsBaru.umur);
+        System.out.println("Jurusan: " + mhsBaru.jurusan);
+    }
+}
